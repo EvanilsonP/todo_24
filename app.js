057fcc18a214e3b7035f4c648c1c9723970769d4
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-19 10:28:50 -0300

    Database connection

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const db = require('./database/db.js');
+db();
 
 app.listen(PORT, () => {
     console.log(`Running on port ${5000}`);
