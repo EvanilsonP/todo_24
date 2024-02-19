c03b4238de7ac5c28281adcf095783301bcd09ff
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-19 10:35:31 -0300

    Creating Todo Schema

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const PORT = process.env.PORT || 5000;
 const db = require('./database/db.js');
 db();
 
+// Midlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejse');
+
 app.listen(PORT, () => {
     console.log(`Running on port ${5000}`);
 });
